26c7ab9c1a247a43e3a3cd5b018779066911443b
Antonio Marsican <antonioeliasrmelo@gmail.com>
2025-04-02 22:29:12 -0300

    implementado com nível mestre

--- super_trunfo.c
+++ super_trunfo.c
@@ -35,6 +35,13 @@ int contarCartasEstado(char sigla[2], struct baralhoTrunfo baralho){
     return 0;
 };
 
+void to_upper(char *str) {
+    while (*str) {
+        *str = toupper((unsigned char) *str);
+        str++;
+    }
+}
+
 struct baralhoTrunfo criarCarta(struct baralhoTrunfo baralho, int qtd) {
     for (int i = 0; i < qtd; i++) {
         int next;
@@ -50,6 +57,8 @@ struct baralhoTrunfo criarCarta(struct baralhoTrunfo baralho, int qtd) {
             }
             
             //function to seach the greatest number, and return if the next number are disponible
+            char estadoUpper[3];
+            to_upper(estado);
             cod = contarCartasEstado(estado,baralho);
             printf("[!]( qtd de cartas com esse estado : %d )\n",cod);
             if (cod + 1 > 4){
@@ -60,45 +69,57 @@ struct baralhoTrunfo criarCarta(struct baralhoTrunfo baralho, int qtd) {
                 baralho.cartas[i].cod = cod + 1;
                 strcpy(baralho.cartas[i].estado,estado);
             }
+           
         }
-
+        getchar();
+        char cidade_temp[34];
 
         printf("Insira o nome da cidade..... ");
-        if(scanf("%33s", baralho.cartas[i].cidade) != 1){
-            printf("Erro ao ler a cidade\n");
+        if (fgets(cidade_temp,33,stdin) != NULL){
+            cidade_temp[strcspn(cidade_temp, "\n")] = '\0';
+            if (strlen(cidade_temp) > 33) {
+                printf("Erro: Cidade excede o tamanho máximo de 33 caracteres.\n");
+                return baralho; // Retorna o baralho sem modificações
+            }
+    
+            // Copiar a cidade para o baralho e converter para maiúsculas
+            strcpy(baralho.cartas[i].cidade, cidade_temp);
+            to_upper(baralho.cartas[i].cidade);
+        }else{
+            printf("Erro ao ler cidade\n");
             return baralho;
         }
+        fflush(stdin);
 
         printf("Insira a populacao..... ");
         if(scanf("%d", &baralho.cartas[i].populacao) != 1){
             printf("Erro ao ler a populacao\n");
             return baralho;
         }
-
+        fflush(stdin);
         printf("Insira a area da cidade(em km²)..... ");
         if(scanf("%f", &baralho.cartas[i].area) != 1){
             printf("Erro ao ler a area\n");
             return baralho;
         }
-
-        printf("Insira o PIB da cidade(em bilhões)..... ");
+        fflush(stdin);
+        printf("Insira o PIB da cidade..... ");
         if(scanf("%f", &baralho.cartas[i].pib) != 1){
             printf("Erro ao ler o PIB\n");
             return baralho;
         }
-
+        fflush(stdin);
         printf("Insira a quantidade de pontos turisticos..... ");
         if(scanf("%d", &baralho.cartas[i].pontosTuristicos) != 1){
             printf("Erro ao ler os pontos turisticos\n");
             return baralho;
-        }
+        }  
+        fflush(stdin);
         
-
-
         printf("===================================================\n");
         //calcular pib per capita e densidade
         baralho.cartas[i].densidade = baralho.cartas[i].populacao / baralho.cartas[i].area;
-        baralho.cartas[i].pibPerCapita = baralho.cartas[i].pib /baralho.cartas[i].populacao;
+        baralho.cartas[i].pibPerCapita = (baralho.cartas[i].pib) /baralho.cartas[i].populacao;
         baralho.qtdCartas++;
     }
     return baralho;
@@ -113,9 +134,9 @@ void mostrarCarta(struct cartaTrunfo carta) {
     printf("Cidade : %s\n", carta.cidade);
     printf("Populacao : %d\n", carta.populacao);
     printf("Pontos Turisticos : %d\n", carta.pontosTuristicos);
-    printf("Area : %.2f km²\n", carta.area);
-    printf("PIB : %.2f bilhões de reais \n", carta.pib);
-    printf("Densidade populacional : %.2f hab/km²\n",carta.densidade);
+    printf("Area : %.2f km2\n", carta.area);
+    printf("PIB : R$ %.2f \n", carta.pib);
+    printf("Densidade populacional : %.2f hab/km2\n",carta.densidade);
     printf("PIB per capita %.2f PIB/hab \n",carta.pibPerCapita);
     printf("===================================================\n");
 }
